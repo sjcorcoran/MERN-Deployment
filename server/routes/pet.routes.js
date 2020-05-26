51c29a40ab3bb580dff49113dd514e968a43eaac
sjcorcoran <seanjcorcoran89@gmail.com>
2020-05-26 13:17:42 -0500

    updated with correct controller name

--- server/routes/pet.routes.js
+++ server/routes/pet.routes.js
@@ -1,12 +1,12 @@
-const ProductController = require("../controllers/Pet.controller");
+const PetController = require("../controllers/pet.controller");
 
 module.exports = (app) => {
-    app.get("/api/pets", ProductController.index); 
-    app.post("/api/pets", ProductController.create);
-    app.get("/api/pets/random", ProductController.random);
-    app.get("/api/pets/:id", ProductController.show);
-    app.delete("/api/pets/:id", ProductController.sell);
-    app.put("/api/pets/:id", ProductController.update);
+    app.get("/api/pets", PetController.index); 
+    app.post("/api/pets", PetController.create);
+    app.get("/api/pets/random", PetController.random);
+    app.get("/api/pets/:id", PetController.show);
+    app.delete("/api/pets/:id", PetController.sell);
+    app.put("/api/pets/:id", PetController.update);
    
   
     
